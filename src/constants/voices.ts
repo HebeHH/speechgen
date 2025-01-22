6f7c8f7449a97e9695df89163f70c342f0ea5eba
hebehh <hebehh@gmail.com>
2025-01-22 16:51:53 +0800

    adding voices

---
+++ src/constants/voices.ts
@@ -0,0 +1,413 @@
+type voice = {
+    id: string;
+    name: string;
+    language: string;
+    gender: 'male' | 'female' | 'neutral';
+}
+
+
+// This only has the English, Hindi and Korean voices
+export const voices: voice[] = [
+    {
+        id: "729651dc-c6c3-4ee5-97fa-350da1f88600",
+        name: "Pleasant Man",
+        language: "EN",
+        gender: 'male'
+    },
+    {
+        id: "e00d0e4c-a5c8-443f-a8a3-473eb9a62355",
+        name: "Friendly Sidekick",
+        language: "EN",
+        gender: 'male'
+    },
+    {
+        id: "39b376fc-488e-4d0c-8b37-e00b72059fdd",
+        name: "Help Desk Man",
+        language: "EN",
+        gender: 'male'
+    },
+    {
+        id: "50d6beb4-80ea-4802-8387-6c948fe84208",
+        name: "The Merchant",
+        language: "EN",
+        gender: 'male'
+    },
+    {
+        id: "d46abd1d-2d02-43e8-819f-51fb652c1c61",
+        name: "Newsman",
+        language: "EN",
+        gender: 'male'
+    },
+    {
+        id: "95856005-0332-41b0-935f-352e296aa0df",
+        name: "Classy British Man",
+        language: "EN",
+        gender: 'male'
+    },
+    {
+        id: "87748186-23bb-4158-a1eb-332911b0b708",
+        name: "Wizardman",
+        language: "EN",
+        gender: 'male'
+    },
+    {
+        id: "a167e0f3-df7e-4d52-a9c3-f949145efdab",
+        name: "Customer support man",
+        language: "EN",
+        gender: 'male'
+    },
+    {
+        id: "41534e16-2966-4c6b-9670-111411def906",
+        name: "1920s Radioman",
+        language: "EN",
+        gender: 'male'
+    },
+    {
+        id: "79f8b5fb-2cc8-479a-80df-29f7a7cf1a3e",
+        name: "Nonfiction Man",
+        language: "EN",
+        gender: 'male'
+    },
+    {
+        id: "f785af04-229c-4a7c-b71b-f3194c7f08bb",
+        name: "John",
+        language: "EN",
+        gender: 'male'
+    },
+    {
+        id: "421b3369-f63f-4b03-8980-37a44df1d4e8",
+        name: "Friendly Australian Man",
+        language: "EN",
+        gender: 'male'
+    },
+    {
+        id: "41f3c367-e0a8-4a85-89e0-c27bae9c9b6d",
+        name: "Australian Customer Support",
+        language: "EN",
+        gender: 'male'
+    },
+    {
+        id: "ee7ea9f8-c0c1-498c-9279-764d6b56d189",
+        name: "Polite Man",
+        language: "EN",
+        gender: 'male'
+    },
+    {
+        id: "bd9120b6-7761-47a6-a446-77ca49132781",
+        name: "Tutorial Man",
+        language: "EN",
+        gender: 'male'
+    },
+    {
+        id: "7360f116-6306-4e9a-b487-1235f35a0f21",
+        name: "Commercial Man",
+        language: "EN",
+        gender: 'male'
+    },
+    {
+        id: "13524ffb-a918-499a-ae97-c98c7c4408c4",
+        name: "Australian Man",
+        language: "EN",
+        gender: 'male'
+    },
+    {
+        id: "820a3788-2b37-4d21-847a-b65d8a68c99a",
+        name: "Salesman",
+        language: "EN",
+        gender: 'male'
+    },
+    {
+        id: "726d5ae5-055f-4c3d-8355-d9677de68937",
+        name: "Kentucky Man",
+        language: "EN",
+        gender: 'male'
+    },
+    {
+        id: "42b39f37-515f-4eee-8546-73e841679c1d",
+        name: "Wise Guide Man",
+        language: "EN",
+        gender: 'male'
+    },
+    {
+        id: "f146dcec-e481-45be-8ad2-96e1e40e7f32",
+        name: "Reading Man",
+        language: "EN",
+        gender: 'male'
+    },
+    {
+        id: "5619d38c-cf51-4d8e-9575-48f61a280413",
+        name: "Announcer Man",
+        language: "EN",
+        gender: 'male'
+    },
+    {
+        id: "f114a467-c40a-4db8-964d-aaba89cd08fa",
+        name: "Yoga Man",
+        language: "EN",
+        gender: 'male'
+    },
+    {
+        id: "a0e99841-438c-4a64-b679-ae501e7d6091",
+        name: "Barbershop Man",
+        language: "EN",
+        gender: 'male'
+    },
+    {
+        id: "ed81fd13-2016-4a49-8fe3-c0d2761695fc",
+        name: "Sportsman",
+        language: "EN",
+        gender: 'male'
+    },
+    {
+        id: "794f9389-aac1-45b6-b726-9d9369183238",
+        name: "Sarah Curious",
+        language: "EN",
+        gender: 'female'
+    },
+    {
+        id: "694f9389-aac1-45b6-b726-9d9369183238",
+        name: "Sarah",
+        language: "EN",
+        gender: 'female'
+    },
+    {
+        id: "156fb8d2-335b-4950-9cb3-a2d33befec77",
+        name: "Helpful Woman",
+        language: "EN",
+        gender: 'female'
+    },
+    {
+        id: "f9836c6e-a0bd-460e-9d3c-f7299fa60f94",
+        name: "Southern Woman",
+        language: "EN",
+        gender: 'female'
+    },
+    {
+        id: "e13cae5c-ec59-4f71-b0a6-266df3c9bb8e",
+        name: "Madame Mischief",
+        language: "EN",
+        gender: 'female'
+    },
+    {
+        id: "af346552-54bf-4c2b-a4d4-9d2820f51b6c",
+        name: "Help Desk Woman",
+        language: "EN",
+        gender: 'female'
+    },
+    {
+        id: "c2ac25f9-ecc4-4f56-9095-651354df60c0",
+        name: "Commercial Lady",
+        language: "EN",
+        gender: 'female'
+    },
+    {
+        id: "c8605446-247c-4d39-acd4-8f4c28aa363c",
+        name: "Wise Lady",
+        language: "EN",
+        gender: 'female'
+    },
+    {
+        id: "00a77add-48d5-4ef6-8157-71e5437b282d",
+        name: "Calm Lady",
+        language: "EN",
+        gender: 'female'
+    },
+    {
+        id: "8985388c-1332-4ce7-8d55-789628aa3df4",
+        name: "Australian Narrator Lady",
+        language: "EN",
+        gender: 'female'
+    },
+    {
+        id: "b7d50908-b17c-442d-ad8d-810c63997ed9",
+        name: "California Girl",
+        language: "EN",
+        gender: 'female'
+    },
+    {
+        id: "996a8b96-4804-46f0-8e05-3fd4ef1a87cd",
+        name: "Storyteller Lady",
+        language: "EN",
+        gender: 'female'
+    },
+    {
+        id: "03496517-369a-4db1-8236-3d3ae459ddf7",
+        name: "ASMR Lady",
+        language: "EN",
+        gender: 'female'
+    },
+    {
+        id: "573e3144-a684-4e72-ac2b-9b2063a50b53",
+        name: "Teacher Lady",
+        language: "EN",
+        gender: 'female'
+    },
+    {
+        id: "a01c369f-6d2d-4185-bc20-b32c225eab70",
+        name: "British Customer Support Lady",
+        language: "EN",
+        gender: 'female'
+    },
+    {
+        id: "8f091740-3df1-4795-8bd9-dc62d88e5131",
+        name: "Princess",
+        language: "EN",
+        gender: 'female'
+    },
+    {
+        id: "043cfc81-d69f-4bee-ae1e-7862cb358650",
+        name: "Australian Woman",
+        language: "EN",
+        gender: 'female'
+    },
+    {
+        id: "829ccd10-f8b3-43cd-b8a0-4aeaa81f3b30",
+        name: "Customer Support Lady",
+        language: "EN",
+        gender: 'female'
+    },
+    {
+        id: "5c42302c-194b-4d0c-ba1a-8cb485c84ab9",
+        name: "Female Nurse",
+        language: "EN",
+        gender: 'female'
+    },
+    {
+        id: "21b81c14-f85b-436d-aff5-43f2e788ecf8",
+        name: "Laidback Woman",
+        language: "EN",
+        gender: 'female'
+    },
+    {
+        id: "4f8651b0-bbbd-46ac-8b37-5168c5923303",
+        name: "Kentucky Woman",
+        language: "EN",
+        gender: 'female'
+    },
+    {
+        id: "11af83e2-23eb-452f-956e-7fee218ccb5c",
+        name: "Midwestern Woman",
+        language: "EN",
+        gender: 'female'
+    },
+    {
+        id: "79a125e8-cd45-4c13-8a67-188112f4dd22",
+        name: "British Lady",
+        language: "EN",
+        gender: 'female'
+    },
+    {
+        id: "bf991597-6c13-47e4-8411-91ec2de5c466",
+        name: "Newslady",
+        language: "EN",
+        gender: 'female'
+    },
+    {
+        id: "bf991597-6c13-47e4-8411-91ec2de5c466",
+        name: "Professional Woman",
+        language: "EN",
+        gender: 'female'
+    },
+    {
+        id: "3b554273-4299-48b9-9aaf-eefd438e3941",
+        name: "Indian Lady",
+        language: "EN",
+        gender: 'female'
+    },
+    {
+        id: "daf747c6-6bc2-4083-bd59-aa94dce23f5d",
+        name: "Middle Eastern Woman",
+        language: "EN",
+        gender: 'female'
+    },
+    {
+        id: "e3827ec5-697a-4b7c-9704-1a23041bbc51",
+        name: "Sweet Lady",
+        language: "EN",
+        gender: 'female'
+    },
+    {
+        id: "15a9cd88-84b0-4a8b-95f2-5d583b54c72e",
+        name: "Reading Lady",
+        language: "EN",
+        gender: 'female'
+    },
+    {
+        id: "34bde396-9fde-4ebf-ad03-e3a1d1155205",
+        name: "New York Woman",
+        language: "EN",
+        gender: 'female'
+    },
+    {
+        id: "71a7ad14-091c-4e8e-a314-022ece01c121",
+        name: "British Reading Lady",
+        language: "EN",
+        gender: 'female'
+    },
+    {
+        id: "4d2fd738-3b3d-4368-957a-bb4805275bd9",
+        name: "British Narration Lady",
+        language: "EN",
+        gender: 'female'
+    },
+    {
+        id: "91b4cf29-5166-44eb-8054-30d40ecc8081",
+        name: "Anna",
+        language: "EN",
+        gender: 'female'
+    },
+    {
+        id: "bdab08ad-4137-4548-b9db-6142854c7525",
+        name: "Hindi Reporter Man",
+        language: "HN",
+        gender: "male",
+    },
+    {
+        id: "ac7ee4fa-25db-420d-bfff-f590d740aeb2",
+        name: "Hindi Calm Man",
+        language: "HN",
+        gender: "male",
+    },
+    {
+        id: "7f423809-0011-4658-ba48-a411f5e516ba",
+        name: "Hindi Narrator Man",
+        language: "HN",
+        gender: "male",
+    },
+    {
+        id: "c1abd502-9231-4558-a054-10ac950c356d",
+        name: "Hindi Narrator Woman",
+        language: "HN",
+        gender: "female",
+    },
+    {
+        id: "95d51f79-c397-46f9-b49a-23763d3eaa2d",
+        name: "Hinglish Speaking Woman",
+        language: "HN",
+        gender: "female",
+    },
+    {
+        id: "304fdbd8-65e6-40d6-ab78-f9d18b9efdf9",
+        name: "Korean Support Woman",
+        language: "KO",
+        gender: "female",
+    },
+    {
+        id: "663afeec-d082-4ab5-827e-2e41bf73a25b",
+        name: "Korean Narrator Woman",
+        language: "KO",
+        gender: "female",
+    },
+    {
+        id: "29e5f8b4-b953-4160-848f-40fae182235b",
+        name: "Korean Calm Woman",
+        language: "KO",
+        gender: "female",
+    },
+    {
+        id: "af6beeea-d732-40b6-8292-73af0035b740",
+        name: "Korean Narrator Man",
+        language: "KO",
+        gender: "male",
+    },
+]
